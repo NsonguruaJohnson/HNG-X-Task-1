c7eb39457eb12d806053067a4e85523b02f3a8b5
NsonguruaJohnson <nsonguruajohnson@gmail.com>
2023-09-08 18:19:15 +0100

    Works

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,18 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('my-info', function (Request $request) {
-    // dd('ok');
-    return response()->json([
-        'slack_name' => $request->query('slack_name'),
-        'current_day' => today()->format('l'),
-        'utc_time' => now(),
-        'track' =>  $request->query('track'),
-        'github_file_url' => 'https://github.com/NsonguruaJohnson/HNG-X-Task-1/blob/main/routes/api.php',
-        'github_repo_url' => 'https://github.com/NsonguruaJohnson/HNG-X-Task-1.git',
-        '“status_code”' => 200,
-    ]);
 });
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('my-info', function (Request $request) {
+    // dd('ok');
+    return response()->json([
+        'slack_name' => $request->query('slack_name'),
+        'current_day' => today()->format('l'),
+        'utc_time' => now(),
+        'track' =>  $request->query('track'),
+        'github_file_url' => 'https://github.com/NsonguruaJohnson/HNG-X-Task-1/blob/main/routes/api.php',
+        'github_repo_url' => 'https://github.com/NsonguruaJohnson/HNG-X-Task-1.git',
+        '“status_code”' => 200,
+    ]);
 });
